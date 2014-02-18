126f05e4ff235f324453dd6644a6143267e00d80
Igor <igor_cfv@hotmail.com>
2014-02-18 15:07:08 +0000

    Adicionado condição de paragem

--- Server/src/Client.java
+++ Server/src/Client.java
@@ -3,22 +3,49 @@ import java.net.*;
 
 class UDPClient
 {
+	private static BufferedReader inFromUser;
+	private static DatagramSocket clientSocket;
+	private static InetAddress IPAddress;
+	static byte[] sendData;
+	static byte[] receiveData;
+	static boolean isRunning = true;
+	
+	
+	public UDPClient()
+	{
+		inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		sendData = new byte[1024];
+		receiveData = new byte[1024];
+	}
+	
 	public static void main(String args[]) throws Exception
 	{
-		BufferedReader inFromUser =
-				new BufferedReader(new InputStreamReader(System.in));
-		DatagramSocket clientSocket = new DatagramSocket();
-		InetAddress IPAddress = InetAddress.getByName("localhost");
-		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
-		String sentence = inFromUser.readLine();
-		sendData = sentence.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-		clientSocket.send(sendPacket);
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		clientSocket.receive(receivePacket);
-		String modifiedSentence = new String(receivePacket.getData());
-		System.out.println("FROM SERVER:" + modifiedSentence);
+		UDPClient client = new UDPClient();
+		client.start("172.30.74.194");
+		
+		while(isRunning)
+		{
+			String sentence = inFromUser.readLine();
+			if(sentence.equals("STOP") || sentence.equals("stop"))
+			{
+				isRunning = false;
+				break;
+			}
+			sendData = sentence.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+			clientSocket.send(sendPacket);
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
+			String modifiedSentence = new String(receivePacket.getData());
+			System.out.println("FROM SERVER:" + modifiedSentence);
+		
+		}
 		clientSocket.close();
+	}
+
+	private void start(String Ip) throws SocketException, UnknownHostException {
+		clientSocket = new DatagramSocket();
+		IPAddress = InetAddress.getByName(Ip);
+		
 	}
 } 	
