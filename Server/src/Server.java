7ac8414b7011783d9a532fae3a7da1fa90755d16
ricardoneves93 <ricardoneves93@hotmail.com>
2014-02-18 22:13:49 +0000

    Tirados os prints de controlo de erros

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -12,7 +12,7 @@ public class Server {
 	private String sentence;
 	private byte[] sendData;
 	private byte[] receiveData;
-	String[] Infos = {"The Instruction isn't well formed", "This data is already stored", "Item sucessefully registed"};
+	String[] Infos = {"The Instruction isn't well formed", "This data is already stored", "Item sucessefully registed", "Wrong plate format"};
 
 	public Server() throws SocketException
 	{
@@ -30,9 +30,8 @@ public class Server {
 			sendData = new byte[1024];
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
-			System.out.println("tamanho buffer: " + receivePacket.getLength());
 			sentence = new String(receivePacket.getData());
-			System.out.println(sentence);
+			System.out.println("Recebido: " + sentence);
 			String info;
 			info = compareData(sentence);
 			InetAddress IpAddress = receivePacket.getAddress();
@@ -52,16 +51,15 @@ public class Server {
 		{
 			if(splitted.length == 3)
 			{
-				if(!splitted[1].matches("([0-9A-Z][0-9A-Z]-){2}[0-9A-Z][0-9A-Z]"))
-					return "Matricula mal formada";
 				if(data.get(splitted[1]) == null && splitted[1].matches("([0-9A-Z][0-9A-Z]-){2}[0-9A-Z][0-9A-Z]"))
 				{
 					data.put(splitted[1].trim(), splitted[2].trim());
 					return Infos[2];
 				}
-				else
-					return Infos[1]; //Is already registed
-
+				else if(!splitted[1].matches("([0-9A-Z][0-9A-Z]-){2}[0-9A-Z][0-9A-Z]"))
+					return Infos[3]; //Wrong plate
+				else if(data.get(splitted[1]) != null)
+					return Infos[2]; //Already registed
 			}
 		}
 		else if(splitted[0].equals("LOOKUP"))
@@ -89,9 +87,6 @@ public class Server {
 
 	public static void main(String[] args) throws Exception
 	{
-		if("AA-AA-AA".matches("([0-9A-Z][0-9A-Z]-){2}[0-9A-Z][0-9A-Z]"))
-			System.out.println("match"); 
-		else System.out.println("nao match");
 		Server myServer = new Server();
 		myServer.start();
 	}
